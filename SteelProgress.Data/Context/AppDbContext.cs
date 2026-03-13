064aefc95247b45632b0f6422715b549ff088c35
samsobao98 <samugranvia@gmail.com>
2026-03-13 18:53:53 +0100

    Configuración de EF Core con SQLite y migración inicial

---
+++ SteelProgress.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SteelProgress.Domain.Entities;
+
+namespace SteelProgress.Data.Context
+{
+    // Usamos DBContext para la conexion entre EF core y la base de datos
+    public class AppDbContext : DbContext 
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ SteelProgress.Data/Factories/AppDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using SteelProgress.Data.Context;
+
+namespace SteelProgress.Data.Factories
+{
+    //Esta clase le dice a EFCore como crear AppDbContext, que proveedor usar y donde estará la base de datos de SQLite
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlite("Data Source=steelprogress.db");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
